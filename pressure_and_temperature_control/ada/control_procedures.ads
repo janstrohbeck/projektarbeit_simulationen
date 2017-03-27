b42163527c65b0010c74fdfdf8a32623a9fafc45
Jan Strohbeck <jan@strohbeck.net>
2017-03-27 08:45:05 +0200

    Small additions.

--- pressure_and_temperature_control/ada/control_procedures.ads
+++ pressure_and_temperature_control/ada/control_procedures.ads
@@ -1,8 +1,12 @@
 with Data_Types; use Data_Types;
 
 package Control_Procedures is
-    -- procedures for converting a reading into
-    -- an appropriate setting for regulation
+    -- Calculates the correct heater setting if the temperature is to be
+    -- regulated to 20 (degrees). Requires the current temperature (TR) and
+    -- stores the result in HS.
     procedure Temp_Convert(TR : TempReading; HS : out HeaterSetting);
+    -- Calculates the correct valve setting if the pressure is to be regulated
+    -- to 1000 (mbar). Requires the current pressure (PR) and stores the result
+    -- in PS.
     procedure Pressure_Convert(PR : PressureReading; PS : out PressureSetting);
 end Control_Procedures;
